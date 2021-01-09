cf452efc98edb397a3cda4b141a12683e5b5899b
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2021-01-09 15:57:06 +0900

    Add Main styled

---
+++ components/layouts/Main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Main() {
+  return (
+    <MainContainer>
+      s
+    </MainContainer>
+  );
+}
+
+const MainContainer = styled.main`
+  background-color:#111;
+`;
+
+export default Main;
